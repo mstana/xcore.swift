b0ed27095bb5c19d68d40894948c375728b438bf
Marek Stana <rstanamarek@gmail.com>
2019-10-24 13:52:59 +0200

    Separate file for Stack selector

--- Sources/Cocoa/Components/Layouts/TileLayout/XCCollectionViewTileStackSelector.swift
+++ Sources/Cocoa/Components/Layouts/TileLayout/XCCollectionViewTileStackSelector.swift
@@ -1,5 +1,5 @@
 //
-// XCCollectionViewTileBackgroundView.swift
+//  XCCollectionViewTileStackSelector.swift
 //
 // Copyright © 2019 Xcore
 //
@@ -24,7 +24,7 @@
 
 import UIKit
 
-final class XCCollectionViewTileBackgroundView: UICollectionReusableView {
+final class XCCollectionViewTileStackSelector: UICollectionReusableView {
     // MARK: - Init Methods
 
     convenience init() {
@@ -41,60 +41,39 @@ final class XCCollectionViewTileBackgroundView: UICollectionReusableView {
         commonInit()
     }
 
-    override class var layerClass: AnyClass {
-        CAShapeLayer.self
-    }
-
-    private var shapeLayer: CAShapeLayer {
-        layer as! CAShapeLayer
-    }
-
-    /// The default value is `.none, 0`.
-    private var corners: (corners: UIRectCorner, radius: CGFloat) = (.none, 0) {
-        didSet {
-            setNeedsLayout()
+    private lazy var leadingButton = UIButton().apply {
+        $0.text = "Show less" // TODO: solve localization
+        $0.action { [weak self] _ in
+            self?.buttonTappedHandler?(.left)
         }
     }
 
-    override var backgroundColor: UIColor? {
-        get { shapeLayer.fillColor?.uiColor }
-        set { shapeLayer.fillColor = newValue?.cgColor }
+    private lazy var trailingButton = UIButton().apply {
+        $0.text = "Clear"  // TODO: solve localization
+        $0.action { [weak self] _ in
+            self?.buttonTappedHandler?(.right)
+        }
     }
+
+    private var buttonTappedHandler: ((XCCollectionViewTileLayoutAction) -> Void)?
 
     private func commonInit() {
-        clipsToBounds = false
-        setupShadow()
-        super.backgroundColor = .clear
-        backgroundColor = .clear
-    }
+        addSubview(leadingButton)
+        addSubview(trailingButton)
 
-    private func setupShadow() {
-        layer.apply {
-            $0.shadowColor = UIColor.black.cgColor
-            $0.shadowOffset = CGSize(width: 0, height: 1)
-            $0.shadowRadius = 1
-            $0.shadowOpacity = 0.3
-            $0.shadowPath = shapeLayer.path
-            $0.shouldRasterize = true
-            $0.rasterizationScale = UIScreen.main.scale
+        leadingButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(.minimumPadding)
         }
-    }
-
-    private func path(rect: CGRect? = nil) -> UIBezierPath {
-        .init(roundedRect: rect ?? bounds, byRoundingCorners: corners.corners, cornerRadii: CGSize(width: corners.radius, height: corners.radius))
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        let path = self.path()
-        shapeLayer.path = path.cgPath
-        layer.shadowPath = path.cgPath
+        trailingButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(.minimumPadding)
+        }
     }
 
     override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
         super.apply(layoutAttributes)
+
         if let tileAttributes = layoutAttributes as? XCCollectionViewTileLayout.Attributes {
-            corners = tileAttributes.corners
+            buttonTappedHandler = tileAttributes.actionHandler
         }
     }
 }
